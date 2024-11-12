23c7e04dd9aa5e98c6e066914885f1a7611473fe
luisrodriguezpalmer <l.rodriguezpalmer001@mymdc.net>
2024-11-11 19:18:25 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+ const apiUrl = 'https://sampleapis.com/api-list/coffee/hot';
+
+ const coffeeContainer = document.getElementById('coffee-container');
+
+ async function fetchCoffeeData() {
+    try {
+        const response = await fetch(apiUrl);
+
+        if (!response.ok) {
+            throw new Error('Coffee data could not be fetched');
+        
+        }
+    
+    const coffeeData = await response.json();
+
+    const coffeeHTML = coffeeData.map(coffee => {
+        return `
+        <div class="coffee-item">
+          <h3>${coffee.title}</h3>
+          <img src="${coffee.image}" alt="${coffee.title}">
+          <p><strong>Ingredients:</strong> ${coffee.ingredients.join(', ')}</p>
+          <p><strong>Description:</strong> ${coffee.description}</p>
+        </div>
+        `;
+    }).join('');
+    
+    coffeeContainer.innerHTML = coffeeHTML;
+    } catch (error) {
+        coffeeContainer.innerHTML = `<p>Error: ${error.message}</p>`;
+    }
+}   
+
+ fetchCoffeeData();
